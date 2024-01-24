d15c608c53ddb49562e023cec3a893cf845b0e19
DS0708 <tlsehdtjd070@naver.com>
2024-01-24 19:56:39 +0900

    add detail page

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const DetailPage = () => {
+  let {movieId} = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    fetchData();
+
+  }, [movieId])
+
+  const fetchData = async () => {
+    const response = await axios.get(`/movie/${movieId}`);
+    setMovie(response.data);
+  };
+
+  if (!movie) return null;
+  
   return (
-    <div>DetailPage</div>
+    <section>
+      <img 
+        className='modal__poster-img'
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} 
+        alt="img" />
+    </section>
   )
 }
 
